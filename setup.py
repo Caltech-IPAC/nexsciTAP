25630aa3fa239cafe02b579665e993b8403cbdc3
John C Good <jcg@ipac.caltech.edu>
2020-05-30 18:16:54 -0700

    Update setup.py; external package name change.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import os
 from setuptools import setup
 from distutils.extension import Extension
 
-setup(name='TAP', 
-    version='1.0.1', 
+setup(name='nexsciTAP', 
+    version='1.1.0', 
     author='Mihseh Kong', 
     author_email='mihseh@ipac.caltech.edu', 
     license='LICENSE.txt', 
     keywords='astronomy, database, web-service', 
     url = 'https://github.com/Caltech-IPAC/TAP',
-    description='VO Table Access Protocol (TAP) web service', 
+    description='NExScI VO Table Access Protocol (TAP) web service', 
     ext_modules=[Extension('writerecs', ['writerecsmodule.c'])],
     install_requires=['ADQL', 'spatial_index'])
