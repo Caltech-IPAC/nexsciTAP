cd66c4b5d71036586d814d60d7387587275f2deb
John C Good <jcg@ipac.caltech.edu>
2020-06-03 13:41:57 -0700

    Update code to use new install path.

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ from setuptools import setup
 from distutils.extension import Extension
 
 setup(name='nexsciTAP', 
-    version='1.1.0', 
+    version='1.1.2', 
     author='Mihseh Kong', 
     author_email='mihseh@ipac.caltech.edu', 
-    license='LICENSE.txt', 
-    keywords='astronomy, database, web-service', 
-    url = 'https://github.com/Caltech-IPAC/TAP',
+    license='LICENSE', 
+    keywords='astronomy database ADQL web-service', 
+    url = 'https://github.com/Caltech-IPAC/nexsciTAP',
     description='NExScI VO Table Access Protocol (TAP) web service', 
-    ext_modules=[Extension('writerecs', ['writerecsmodule.c'])],
-    install_requires=['ADQL', 'spatial_index'])
+    long_description=open('README.md').read(),
+    ext_modules=[Extension('TAP/writerecs', ['TAP/writerecsmodule.c'])],
+    install_requires=['ADQL', 'spatial_index', 'configobj', 'cx_Oracle'],
+    packages=['TAP']
+)
