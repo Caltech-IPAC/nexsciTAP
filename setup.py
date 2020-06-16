937a1b76a09f6aeb71f53cb72f4c8affe91af21b
John C Good <jcg@ipac.caltech.edu>
2020-06-16 12:31:45 -0700

    Document use of convex polygons.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from distutils.extension import Extension
 
 setup(name='nexsciTAP', 
-    version='1.1.2', 
+    version='1.1.3', 
     author='Mihseh Kong', 
     author_email='mihseh@ipac.caltech.edu', 
     license='LICENSE', 
